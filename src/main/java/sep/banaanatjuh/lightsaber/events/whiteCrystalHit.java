ecef186e166d2bc21cca451ceef2e85191d12c13
sep-raes <113251369+sep-raes@users.noreply.github.com>
2025-12-05 13:15:07 +0100

    WhiteCrystal to RedCrystal

--- src/main/java/sep/banaanatjuh/lightsaber/events/whiteCrystalHit.java
+++ src/main/java/sep/banaanatjuh/lightsaber/events/whiteCrystalHit.java
@@ -3,6 +3,7 @@ package sep.banaanatjuh.lightsaber.events;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -16,20 +17,25 @@ public class whiteCrystalHit {
         ServerLivingEntityEvents.AFTER_DEATH.register((entity, source) -> {
             if (source.getAttacker() instanceof PlayerEntity player) {
 
-
+                PlayerInventory playerInventory = player.getInventory();
                 ItemStack offHand = player.getStackInHand(Hand.OFF_HAND);
 
-                int count = offHand.getCount();
 
-                ItemStack redCrystal = new ItemStack(ModItems.RED_CRYSTAL);
-                redCrystal.setCount(count);
+
 
                 if (offHand.getItem() == ModItems.WHITE_CRYSTAL) {
 
+                    int count = offHand.getCount();
+                    ItemStack redCrystal = new ItemStack(ModItems.RED_CRYSTAL);
+
                     LOGGER.info("Killed an entity while holding white crystal");
-                    player.setStackInHand(Hand.OFF_HAND, redCrystal);
+
+                    playerInventory.offerOrDrop(redCrystal);
+                    offHand.setCount(count - 1);
 
                 }
+
+
             }
         });
     }
